4f5962329f9115a1f78e469d18c460e1b86962ed
msmithivas <msmithivas@gmail.com>
2023-02-26 05:14:20 +0000

    Initial commit

--- app/controllers/fire_controller.rb
+++ app/controllers/fire_controller.rb
@@ -1,4 +1,24 @@
 class FireController < ApplicationController
+  
+  def horoscopes
+
+   all_zodiacs = Zodiac.list
+   @this_zodiac = all_zodiacs.fetch(params.fetch("the_sign".to_sym))
+   @horoscope = this_zodiac.fetch(:horoscope)
+
+
+   @array_of_numbers = Array.new
+
+    5.times do
+      another_number = rand(1...100)
+      
+      @array_of_numbers.push(another_number)
+    end
+
+   render({ :template => "flame_templates/all_zodiac.html.erb" })
+  end
+
+  =begin
   def ram
     @horoscope = "As your professional dreams unfold, Aries, you may worry about the downside. First, there are new responsibilities that you might doubt your ability to fulfill. Second, you might be catapulted into an uncomfortable new realm of office politics. Don't let these matters put a damper on your enthusiasm. You have what it takes to fulfill the first concern and the wisdom to avoid the second.
      Onward and upward."
@@ -41,4 +61,6 @@ class FireController < ApplicationController
 
     render({ :template => "flame_templates/sagittarius.html.erb" })
   end
+=end
+
 end
